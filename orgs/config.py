19519ff41901754d5db3e91576f795ad2fe9479f
Hau Nguyen <hnguyen1@hnguyen1-m01.ad.savagebeast.com>
2019-08-06 00:32:01 -0700

    third

---
+++ orgs/config.py
@@ -0,0 +1,10 @@
+import os
+
+class Config:
+    SECRET_KEY = '083asdas01803das0a89040a49a0a00asdca'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from orgs import app
+from orgs import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
         app.run(debug=True)
