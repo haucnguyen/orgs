7ec0047d80257631ea515c308ef4cd7c1e4d17d6
Hau Nguyen <hnguyen1@hnguyen1-m01.ad.savagebeast.com>
2019-08-06 10:51:30 -0700

    third

--- orgs/main/routes.py
+++ orgs/main/routes.py
@@ -1,11 +1,16 @@
 from flask import render_template, request, Blueprint
+from flask_login import login_required
 from orgs.models import Post
 
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
+def landingpage():
+        return render_template('landingpage.html', title='Welcome to Orgs')
+
 @main.route("/home")
+@login_required
 def home():
         page = request.args.get('page', 1, type=int)
         posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
@@ -13,5 +18,6 @@ def home():
 
 
 @main.route("/about")
+@login_required
 def about():
         return render_template('about.html', title='about')
--- orgs/posts/routes.py
+++ orgs/posts/routes.py
@@ -18,7 +18,7 @@ def new_post():
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('main.home'))
-    return render_template('create_post.html', title='New Post', form=form, legend='New Post')
+    return render_template('create_post.html', title='New Event', form=form, legend='New Event')
 
 
 @posts.route("/post/<int:post_id>")
@@ -42,7 +42,7 @@ def update_post(post_id):
         elif request.method == 'GET':
                 form.title.data = post.title
                 form.content.data = post.content
-        return render_template('create_post.html', title='Update Post', form=form, legend='Update Post')
+        return render_template('create_post.html', title='Update Event', form=form, legend='Update Event')
 
 
 @posts.route("/post/<int:post_id>/delete", methods=['POST'])
